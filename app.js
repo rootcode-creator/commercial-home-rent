333796f09329c067cda6f9362944c0b4443d14ee
Kawser Ahmad <bestpdf4364@gmail.com>
2026-04-09 15:27:33 +0600

    Add selectedCategory to local variables for improved search functionality

--- app.js
+++ app.js
@@ -134,6 +134,7 @@ app.use( (req, res, next) => {
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   res.locals.searchQuery = req.query?.q || "";
+  res.locals.selectedCategory = req.query?.category || "";
   next();
 });
 
